374b0462179dab75d0890ffbfd8797aa2f0ec358
Joseph Chen <phenomenon0102@gmail.com>
2013-10-11 20:38:36 +0800

    default utf-8 encoding for new file

--- init-local.el
+++ init-local.el
@@ -5,7 +5,12 @@
 
 ; set font size
 (when *win32*
-(set-face-attribute 'default nil :height 140)
-)
+  (set-face-attribute 'default nil :height 140)
+  (setenv "LANG" "zh_TW.UTF-8")
+  )
+
+; default utf-8
+(prefer-coding-system 'utf-8-unix)
+(setq buffer-file-coding-system 'utf-8-unix)
 
 (provide 'init-local)
